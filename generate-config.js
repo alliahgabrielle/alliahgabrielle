a116b269ee96aa9bee5a43272dd4914baf6169fd
Alliah Gabrielle Galang <alliahgabrielle@gmail.com>
2026-05-23 11:44:52 +0800

    Email Test

--- generate-config.js
+++ generate-config.js
@@ -4,5 +4,5 @@ const content = `const EMAILJS_PUBLIC_KEY  = '${process.env.EMAILJS_PUBLIC_KEY}'
 const EMAILJS_SERVICE_ID  = '${process.env.EMAILJS_SERVICE_ID}';
 const EMAILJS_TEMPLATE_ID = '${process.env.EMAILJS_TEMPLATE_ID}';`;
 
-fs.writeFileSync('./js/config.js', content);
+fs.writeFileSync('./public/js/config.js', content);
 console.log('✓ config.js generated');
