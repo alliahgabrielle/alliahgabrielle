e7a6ef399da9bbd8ccddff29894310f1e88237be
Alliah Gabrielle Galang <alliahgabrielle@gmail.com>
2026-05-23 11:29:08 +0800

    Vercel Test

--- public/generate-config.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const config = `const EMAILJS_PUBLIC_KEY  = '${process.env.EMAILJS_PUBLIC_KEY}';
-const EMAILJS_SERVICE_ID  = '${process.env.EMAILJS_SERVICE_ID}';
-const EMAILJS_TEMPLATE_ID = '${process.env.EMAILJS_TEMPLATE_ID}';`;
-
-fs.writeFileSync('./js/config.js', config);
-console.log('config.js generated successfully');
